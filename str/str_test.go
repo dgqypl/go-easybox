6e2ba3bb36113e240f47998d413727aaf235a9f0
Mew151 <dgqypl@gmail.com>
2022-08-04 18:11:38 +0800

    str包增加单元测试

--- str/str_test.go
+++ str/str_test.go
@@ -2,6 +2,7 @@ package str
 
 import (
 	easyboxt "github.com/dgqypl/go-easybox/testing"
+	"strconv"
 	"testing"
 )
 
@@ -13,4 +14,16 @@ type int64ToStringCase struct {
 func TestInt64ToString(t *testing.T) {
 	easyboxt.FuncAssertion(t, &int64ToStringCase{10, "10"}, Int64ToString)
 	easyboxt.FuncAssertion(t, &int64ToStringCase{-1, "-1"}, Int64ToString)
+}
+
+type stringToInt64Case struct {
+	In  string
+	Out int64
+	Err error
+}
+
+func TestStringToInt64(t *testing.T) {
+	easyboxt.FuncAssertion(t, &stringToInt64Case{"10", 10, nil}, StringToInt64)
+	easyboxt.FuncAssertion(t, &stringToInt64Case{"-1", -1, nil}, StringToInt64)
+	easyboxt.FuncAssertion(t, &stringToInt64Case{"a", 0, &strconv.NumError{Func: "ParseInt", Num: "a", Err: strconv.ErrSyntax}}, StringToInt64)
 }
